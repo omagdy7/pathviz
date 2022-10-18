be535e24d4932843aa46930e813098f7f32810c5
omagdy7 <omar.professional8777@gmail.com>
2022-10-18 13:04:24 +0200

    Succesfully made dfs working and trying to get bfs working as well

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 #![allow(unused_imports)]
 
-use egui::RichText;
-use macroquad::{prelude::*, color};
+use std::{thread, time::Duration, collections::VecDeque};
+
+use egui::{RichText, color::HsvaGamma};
 use egui_macroquad;
+use macroquad::{color, prelude::*};
 
-
-pub const START_COLOR: Color = color::colors::MAGENTA;
+pub const START_COLOR: Color  = color::colors::MAGENTA;
 pub const TARGET_COLOR: Color = color::colors::RED;
-pub const WALL_COLOR: Color = color::colors::YELLOW;
-pub const VIS_COLOR: Color = color::colors::PURPLE;
-pub const NONVIS_COLOR: Color = color::colors::WHITE;
+pub const WALL_COLOR: Color   = color::colors::YELLOW;
+pub const VIS_COLOR: Color    = color::colors::PURPLE;
+pub const NONVIS_COLOR: Color = color::colors::DARKGRAY;
 pub const BUTTON_WIDTH: f32 = 30.0;
-pub const TOP_PANEL_HEIGHT : f32 = 60.0;
+pub const TOP_PANEL_HEIGHT: f32 = 60.0;
 pub const RECT_WIDTH: f32 = 20.0;
 pub const SCREEN_WIDTH: f32 = 1920.0;
 pub const SCREEN_HEIGHT: f32 = 1080.0 - TOP_PANEL_HEIGHT;
@@ -36,32 +37,145 @@ pub enum MyButton {
     Wall,
 }
 
+#[derive(PartialEq, Debug)]
+pub enum State {
+    Playing,
+    Paused,
+    TargetFound,
+}
 
 pub struct Solver {
     pub grid: Vec<Vec<Rect>>,
     pub start: Option<(usize, usize)>,
     pub target: Option<(usize, usize)>,
+    pub last: VecDeque<(usize, usize)>
 }
 
 impl Solver {
-    pub fn new(grid : Vec<Vec<Rect>>, start: Option<(usize, usize)>, target: Option<(usize, usize)>) -> Self {
+    pub fn new(
+        grid: Vec<Vec<Rect>>,
+        start: Option<(usize, usize)>,
+        target: Option<(usize, usize)>,
+        last: VecDeque<(usize, usize)>
+    ) -> Self {
         Self {
             grid,
             start,
             target,
+            last,
         }
     }
 
-    pub fn dfs(&mut self) {
-        todo!();
+    pub fn set_last(&mut self, value: Option<(usize, usize)>) {
+        self.last.push_back(value.unwrap());
     }
 
-    pub fn bfs(&mut self) {
-        todo!();
+    pub fn dfs(&mut self, r: usize, c: usize, state: &mut State) {
+        self.grid[r][c].color = VIS_COLOR;
+        draw_rectangle(r as f32 * RECT_WIDTH,(c as f32 * RECT_WIDTH) + TOP_PANEL_HEIGHT, RECT_WIDTH, RECT_WIDTH, VIS_COLOR);
+        // draw_rectangle_lines(r as f32 * RECT_WIDTH, (c as f32 * RECT_WIDTH) + TOP_PANEL_HEIGHT, RECT_WIDTH, RECT_WIDTH, 2.0, VIS_COLOR);
+        // thread::sleep(Duration::from_millis(10));
+        self.last.pop_back();
+
+
+        let dx: [i32; 4] = [0, 1,  0, -1];
+        let dy: [i32; 4] = [-1, 0, 1,  0];
+
+        for i in 0..4 {
+            let nx: i32 = r as i32 + dx[i];
+            let ny: i32 = c as i32 + dy[i];
+            if self.is_valid_idx(nx, ny) && self.grid[nx as usize][ny as usize].color == TARGET_COLOR {
+                self.last.clear();
+                *state = State::TargetFound;
+            }
+
+            if self.is_valid_idx(nx, ny)
+                && self.grid[nx as usize][ny as usize].color == NONVIS_COLOR
+            {
+                self.set_last(Some((nx as usize, ny as usize)));
+            }
+        }
     }
 
-    pub fn mark(&mut self, (x, y) : (f32, f32), color: Color) {
-        self.grid[(x / RECT_WIDTH) as usize + 1][((y - TOP_PANEL_HEIGHT) / RECT_WIDTH) as usize].color = color;
+    pub fn bfs(&mut self, r: usize, c: usize, state: &mut State) {
+        self.grid[r][c].color = VIS_COLOR;
+        draw_rectangle(r as f32 * RECT_WIDTH,(c as f32 * RECT_WIDTH) + TOP_PANEL_HEIGHT, RECT_WIDTH, RECT_WIDTH, VIS_COLOR);
+        // draw_rectangle_lines(r as f32 * RECT_WIDTH, (c as f32 * RECT_WIDTH) + TOP_PANEL_HEIGHT, RECT_WIDTH, RECT_WIDTH, 2.0, VIS_COLOR);
+        // thread::sleep(Duration::from_millis(10));
+
+        self.last.pop_front();
+
+        let dx: [i32; 4] = [0, 1,  0, -1];
+        let dy: [i32; 4] = [-1, 0, 1,  0];
+
+        for i in 0..4 {
+            let nx: i32 = r as i32 + dx[i];
+            let ny: i32 = c as i32 + dy[i];
+            if self.is_valid_idx(nx, ny) && self.grid[nx as usize][ny as usize].color == TARGET_COLOR {
+                self.last.clear();
+                *state = State::TargetFound;
+            }
+
+            if self.is_valid_idx(nx, ny)
+                && self.grid[nx as usize][ny as usize].color == NONVIS_COLOR
+            {
+                self.last.push_front((nx as usize, ny as usize));
+            }
+        }
+    }
+
+    pub fn mark(&mut self, (x, y): (f32, f32), color: Color) {
+        let r = (x / RECT_WIDTH) as usize;
+        let c = ((y - TOP_PANEL_HEIGHT) / RECT_WIDTH) as usize;
+        if color == START_COLOR {
+            self.reset_start();
+            self.start = Some((r, c));
+            self.last.clear();
+            self.last.push_back((r, c))
+        } else if color == TARGET_COLOR {
+            self.reset_target();
+            self.target = Some((r, c));
+        }
+
+        // if self.is_safe_to_mark((r, c), color) {
+        self.grid[r][c].color = color;
+        // }
+    }
+
+    fn is_valid_idx(&self, i: i32, j: i32) -> bool {
+        i >= 0 && j >= 0 && i < self.grid.len() as i32 && j < self.grid[0].len() as i32
+    }
+
+    pub fn draw(&mut self, selected_algo: &Algorithm, game_state: &mut State) {
+        if let State::Playing = game_state {
+            let (x, y) = self.last.back().unwrap();
+            let (xb, yb) = self.last.front().unwrap();
+            match selected_algo {
+                Algorithm::Dfs => self.dfs(*x, *y, game_state),
+                Algorithm::Bfs => self.bfs(*xb, *yb, game_state),
+            }
+        }
+
+        for row in self.grid.iter() {
+            for rect in row.iter() {
+                draw_rectangle(rect.x, rect.y, rect.w, rect.h, rect.color);
+                draw_rectangle_lines(rect.x, rect.y, rect.w, rect.h, 2.0, BLACK);
+            }
+        }
+    }
+
+    fn reset_start(&mut self) {
+        if let Some(st) = self.start {
+            self.grid[st.0][st.1].color = NONVIS_COLOR;
+            self.start = None;
+        }
+    }
+
+    fn reset_target(&mut self) {
+        if let Some(end) = self.target {
+            self.grid[end.0][end.1].color = NONVIS_COLOR;
+            self.target = None;
+        }
     }
 }
 
@@ -73,18 +187,8 @@ pub struct Rect {
     pub color: Color,
 }
 
-pub fn sdf() {
-}
-
 impl Rect {
     pub fn new(x: f32, y: f32, w: f32, h: f32, color: Color) -> Self {
-        Rect {
-            x,
-            y,
-            w,
-            h,
-            color
-        }
+        Rect { x, y, w, h, color }
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,48 @@
+use std::collections::VecDeque;
+
 use egui::RichText;
-use macroquad::prelude::*;
 use egui_macroquad;
+use macroquad::prelude::*;
 
 use pathviz::*;
 
 // use egui::Window;
 
-
 #[macroquad::main("egui with macroquad")]
 async fn main() {
-    let c = SCREEN_WIDTH / RECT_WIDTH;
-    let r = SCREEN_HEIGHT / RECT_WIDTH;
-    let mut grid : Vec<Vec<pathviz::Rect>> = vec![vec![]];
-    for i in 0..=c as usize {
-        let mut col : Vec<pathviz::Rect> = vec![];
-        for j in 0..=r as usize {
-            let rect = pathviz::Rect::new(i as f32 * RECT_WIDTH , (j as f32 * RECT_WIDTH) + TOP_PANEL_HEIGHT, RECT_WIDTH, RECT_WIDTH, WHITE);
+    let r = SCREEN_WIDTH / RECT_WIDTH;
+    let c = SCREEN_HEIGHT / RECT_WIDTH;
+    let mut grid: Vec<Vec<pathviz::Rect>> = Vec::new();
+    for i in 0..r as usize {
+        let mut col: Vec<pathviz::Rect> = vec![];
+        for j in 0..c as usize {
+            let rect = pathviz::Rect::new(
+                i as f32 * RECT_WIDTH,
+                (j as f32 * RECT_WIDTH) + TOP_PANEL_HEIGHT,
+                RECT_WIDTH,
+                RECT_WIDTH,
+                NONVIS_COLOR,
+            );
             col.push(rect);
         }
         grid.push(col);
     }
+    let mut st: VecDeque<(usize, usize)> = VecDeque::new();
+    st.push_back((0, 0));
 
-    let mut solver = Solver::new(grid, (0, 0), (50, 50));
+    let mut solver = Solver::new(
+        grid,
+        Some((0, 0)),
+        Some(((r - 1.0) as usize, (c - 1.0) as usize)),
+        st,
+    );
 
-
+    let mut game_state = State::Paused;
     let mut selected_algo = Algorithm::Dfs;
     let mut selected_speed = Speed::Average;
     let mut cur_button = MyButton::Start;
     let mut x_pos = 0.0;
     let mut y_pos = 0.0;
-
 
     loop {
         clear_background(BLACK);
@@ -40,77 +53,110 @@ async fn main() {
             x_pos = x;
             y_pos = y;
             match cur_button {
-                MyButton::Start => if y >= TOP_PANEL_HEIGHT { solver.mark((x, y), START_COLOR) },
-                MyButton::Target => if y >= TOP_PANEL_HEIGHT { solver.mark((x, y), TARGET_COLOR) },
-                MyButton::Wall => if y >= TOP_PANEL_HEIGHT { solver.mark((x, y), WALL_COLOR) },
+                MyButton::Start => {
+                    if y >= TOP_PANEL_HEIGHT {
+                        solver.mark((x, y), START_COLOR)
+                    }
+                }
+                MyButton::Target => {
+                    if y >= TOP_PANEL_HEIGHT {
+                        solver.mark((x, y), TARGET_COLOR)
+                    }
+                }
+                MyButton::Wall => {
+                    if y >= TOP_PANEL_HEIGHT {
+                        solver.mark((x, y), WALL_COLOR)
+                    }
+                }
             }
         }
 
         egui_macroquad::ui(|egui_ctx| {
-            egui::TopBottomPanel::top("Demo panel").resizable(true).min_height(TOP_PANEL_HEIGHT).show(egui_ctx, |ui| {
-                ui.horizontal(|ui| {
-                    if ui.button(RichText::new("Start Node").size(BUTTON_WIDTH)).clicked() {
-                        cur_button = MyButton::Start;
-                    }
-                    if ui.button(RichText::new("Target Node").size(BUTTON_WIDTH)).clicked() {
-                        cur_button = MyButton::Target;
-                    }
-                    if ui.button(RichText::new("Draw Walls").size(BUTTON_WIDTH)).clicked() {
-                        cur_button = MyButton::Wall;
-                    }
-                    if ui.button(RichText::new("Play").size(BUTTON_WIDTH)).clicked() {
-                        match selected_algo {
-                            Algorithm::Dfs => solver.dfs(),
-                            Algorithm::Bfs => solver.bfs(),
+            egui::TopBottomPanel::top("Demo panel")
+                .resizable(true)
+                .min_height(TOP_PANEL_HEIGHT)
+                .show(egui_ctx, |ui| {
+                    ui.horizontal(|ui| {
+                        if ui
+                            .button(RichText::new("Start Node").size(BUTTON_WIDTH))
+                            .clicked()
+                        {
+                            cur_button = MyButton::Start;
                         }
-                    }
-                    if ui.button(RichText::new("Pause").size(BUTTON_WIDTH)).clicked() {
-                    }
-                    egui::ComboBox::from_label(RichText::new("Speed").size(20.0))
-                        .selected_text(format!("{:?}", selected_speed)).width(20.0)
-                        .show_ui(ui, |ui| {
-                            ui.selectable_value(&mut selected_speed, Speed::Slow, RichText::new("Slow").size(20.0));
-                            ui.selectable_value(&mut selected_speed, Speed::Average, RichText::new("Average").size(20.0));
-                            ui.selectable_value(&mut selected_speed, Speed::Fast, RichText::new("Fast").size(20.0));
+                        if ui
+                            .button(RichText::new("Target Node").size(BUTTON_WIDTH))
+                            .clicked()
+                        {
+                            cur_button = MyButton::Target;
                         }
-                    );
-                    egui::ComboBox::from_label("Algorithms")
-                        .selected_text(format!("{:?}", selected_algo))
-                        .show_ui(ui, |ui| {
-                            ui.selectable_value(&mut selected_algo, Algorithm::Dfs, "DFS");
-                            ui.selectable_value(&mut selected_algo, Algorithm::Bfs, "BFS");
+                        if ui
+                            .button(RichText::new("Draw Walls").size(BUTTON_WIDTH))
+                            .clicked()
+                        {
+                            cur_button = MyButton::Wall;
                         }
-                    );
-                })
-            });
-            egui::Window::new("Debug window").resizable(true).show(egui_ctx, |ui| {
-                ui.label(format!("x: {}", x_pos));
-                ui.label(format!("y: {}", y_pos));
-                ui.label(format!("currrent button: {:?}", cur_button));
-                ui.label(format!("selected algo: {:?}", selected_algo));
-                ui.label(format!("selected speed: {:?}", selected_speed));
-            });
+                        if ui
+                            .button(RichText::new("Play").size(BUTTON_WIDTH))
+                            .clicked()
+                        {
+                            game_state = State::Playing;
+                        }
+                        if ui
+                            .button(RichText::new("Pause").size(BUTTON_WIDTH))
+                            .clicked()
+                        {
+                            game_state = State::Paused;
+                        }
+                        egui::ComboBox::from_label(RichText::new("Speed").size(20.0))
+                            .selected_text(format!("{:?}", selected_speed))
+                            .width(20.0)
+                            .show_ui(ui, |ui| {
+                                ui.selectable_value(
+                                    &mut selected_speed,
+                                    Speed::Slow,
+                                    RichText::new("Slow").size(20.0),
+                                );
+                                ui.selectable_value(
+                                    &mut selected_speed,
+                                    Speed::Average,
+                                    RichText::new("Average").size(20.0),
+                                );
+                                ui.selectable_value(
+                                    &mut selected_speed,
+                                    Speed::Fast,
+                                    RichText::new("Fast").size(20.0),
+                                );
+                            });
+                        egui::ComboBox::from_label("Algorithms")
+                            .selected_text(format!("{:?}", selected_algo))
+                            .show_ui(ui, |ui| {
+                                ui.selectable_value(&mut selected_algo, Algorithm::Dfs, "DFS");
+                                ui.selectable_value(&mut selected_algo, Algorithm::Bfs, "BFS");
+                            });
+                    })
+                });
+            egui::Window::new("Debug window")
+                .resizable(true)
+                .show(egui_ctx, |ui| {
+                    ui.label(format!("x: {}", x_pos));
+                    ui.label(format!("y: {}", y_pos));
+                    ui.label(format!("start: {:?}", solver.start.unwrap()));
+                    ui.label(format!("target: {:?}", solver.target.unwrap()));
+                    // ui.label(format!("last: {:?}", solver.last.back()));
+                    ui.label(format!("currrent button: {:?}", cur_button));
+                    ui.label(format!("selected algo: {:?}", selected_algo));
+                    ui.label(format!("selected speed: {:?}", selected_speed));
+                });
         });
 
         // Draw things before egui
 
+        solver.draw(&selected_algo, &mut game_state);
 
-        for row in solver.grid.iter() {
-            for rect in row.iter() {
-                draw_rectangle(rect.x, rect.y, rect.w, rect.h, rect.color);
-                draw_rectangle_lines(rect.x, rect.y, rect.w, rect.h, 2.0, GOLD);
-            }
-        }
         egui_macroquad::draw();
 
-
-
-        
         // Draw things after egui
 
         next_frame().await;
-
-
     }
 }
-
